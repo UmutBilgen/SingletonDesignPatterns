5cf9584fc70ad228ed1de1ec030d546b1a54289d
Umut Bilgen <84351356+UmutBilgen@users.noreply.github.com>
2022-01-17 18:43:26 +0300

    Singleton Tasarım Kalıbı Örnek
    Singleton Tasarım Kalıbı Örnek

---
+++ SingletonTasarım Kalıbı/SingletonTasarım Kalıbı/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonTasarım_Kalıbı
+{
+    class Singleton
+    {
+        private static Singleton instance;
+         public string ad { get; set; }
+        
+        private Singleton()
+        {
+                 ad= "umut";
+        }
+        
+        public static Singleton GetSingleton()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            else
+            {
+                instance.ad = "kopya";
+            }
+            return instance;
+        }
+    }
+}
